a311f3f28f6a598058521f737ceb907ef5fb7c8a
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-03-13 11:16:56 +0100

    fix add system.in

--- src/org/lessons/java/PasswordGenerator.java
+++ src/org/lessons/java/PasswordGenerator.java
@@ -1,5 +1,7 @@
 package src.org.lessons.java;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
     public static void main(String[] args) {
@@ -9,16 +11,32 @@ public class PasswordGenerator {
         int dataGiorno;
         int dataMese;
         int dataAnno;
-        nome = "Antonio";
-        cognome = "Rossi";
-        colorePreferito = "blu";
-        dataGiorno = 20;
-        dataMese = 12;
-        dataAnno = 2024;
+        // nome = "Antonio";
+        // cognome = "Rossi";
+        // colorePreferito = "blu";
+        // dataGiorno = 20;
+        // dataMese = 12;
+        // dataAnno = 2024;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("scrivi nome utente: ");
+        nome = input.nextLine();
+        
+        System.out.println("scrivi cognome utente: ");
+        cognome = input.nextLine();
+        
+        System.out.println("scrivi colore utente: ");
+        colorePreferito = input.nextLine();
+        
+        System.out.println("scrivi in successione con un invio; giorno, mese, anno: ");
+        dataGiorno = input.nextInt();
+        dataMese = input.nextInt();
+        dataAnno = input.nextInt();
+        
         int dataSomma = dataGiorno + dataMese + dataAnno;
+
         System.out.println("Il mio utente si chiama " + nome + " " + cognome + ", " + "nato il " + dataGiorno + "/"
                 + dataMese + "/" + dataAnno + " il cui colore preferito è il " + colorePreferito + ".");
         System.out.println("La sua password sarà " + nome + "-" + cognome + "-" + colorePreferito + "-" + dataSomma);
     }
 }
-
